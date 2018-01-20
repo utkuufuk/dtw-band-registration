e24b63507e07522c208cacf0eef6bd0958444b33
utkuufuk <utkuufuk@gmail.com>
2018-01-20 18:37:36 +0300

    don't let reference rows & cols be too close to each other

--- find_ref_cols.m
+++ find_ref_cols.m
@@ -3,11 +3,11 @@ function refCols = find_ref_cols(dwtHigh, numRefCols, margin, resolution)
     endCol = size(dwtHigh, 2) - margin;
     colFreqs = sum(abs(dwtHigh(:, startCol:endCol)), 1);
     
-%     for c = 1:size(dwtHigh, 2)
-%        if rem(c, resolution) ~= 0
-%            colFreqs(c) = 0;
-%        end
-%     end
+    for c = 1:size(dwtHigh, 2)
+       if rem(c, resolution) ~= 0
+           colFreqs(c) = 0;
+       end
+    end
     
     [~, indices] = sort(colFreqs, 'descend');
     refCols = indices(1:numRefCols) + margin;
--- find_ref_rows.m
+++ find_ref_rows.m
@@ -3,11 +3,11 @@ function refRows = find_ref_rows(dwtHigh, numRefRows, margin, resolution)
     endRow = size(dwtHigh, 1) - margin;
     rowFreqs = sum(abs(dwtHigh(startRow:endRow, :)), 2);
     
-%     for r = 1:size(dwtHigh, 1)
-%        if rem(r, resolution) ~= 0
-%            rowFreqs(r) = 0;
-%        end
-%     end
+    for r = 1:size(dwtHigh, 1)
+       if rem(r, resolution) ~= 0
+           rowFreqs(r) = 0;
+       end
+    end
     
     [~, indices] = sort(rowFreqs, 'descend');
     refRows = indices(1:numRefRows) + margin;
