c6e0ec5aaf849cb3d66012b29dd6ed1620248b57
utku.ufuk <utku.ufuk@uzay.tubitak.gov.tr>
2018-01-31 11:27:54 +0300

    produce only 1 pansharpened image per DTW output

--- pansharp/pansharp.m
+++ pansharp/pansharp.m
@@ -1,7 +1,7 @@
 clc, clearvars
 
 % number of test images
-NUM_IMAGES = 16;
+NUM_IMAGES = 10;
 
 % number of multispectral image bands
 NUM_MSI_BANDS = 3;
@@ -9,69 +9,39 @@ NUM_MSI_BANDS = 3;
 % image dimensions
 NUM_ROWS_DTW = 8192;
 NUM_ROWS_SIFT = 8128;
-NUM_COLS_L0 = 8192;
-NUM_COLS_L1 = 8160;
+NUM_COLS = 8160;
 
 % maximum shifted rows/cols 
-MARGIN = 60;
+ROW_MARGIN = 50;
+COL_MARGIN = 10;
 
 % number of reference rows & cols in each iteration
-NUM_REF_ROWS_COLS = linspace(1, 25, 5);
+NUM_REF_ROWS_COLS = linspace(5, 45, 5);
 
 % create blank images to store the msi bands
-msiDtwSimpleL0 = zeros(NUM_ROWS_DTW, NUM_COLS_L0, NUM_MSI_BANDS);
-msiDtwSimpleL1 = zeros(NUM_ROWS_DTW, NUM_COLS_L1, NUM_MSI_BANDS);
-msiDtwEnhancedL0 = zeros(NUM_ROWS_DTW, NUM_COLS_L0, NUM_MSI_BANDS);
-msiDtwEnhancedL1 = zeros(NUM_ROWS_DTW, NUM_COLS_L1, NUM_MSI_BANDS);
-msiSift = zeros(NUM_ROWS_SIFT, NUM_COLS_L1, NUM_MSI_BANDS);
+msiDtw = zeros(NUM_ROWS_DTW, NUM_COLS, NUM_MSI_BANDS);
+msiSift = zeros(NUM_ROWS_SIFT, NUM_COLS, NUM_MSI_BANDS);
 
-for i = 11:NUM_IMAGES
-    % read L0, L1 and L1R pan images
-    panImageNameL0 = strcat('../images/', num2str(i), '/L0/0/image.tif');
-    panImageNameL1 = strcat('../images/', num2str(i), '/L1/0/image.tif');
-    panImageNameL1R = strcat('../images/', num2str(i), '/L1R/0/image.tif');
-    panL0 = imread(panImageNameL0);
-    panL1 = imread(panImageNameL1);
-    panL1R = imread(panImageNameL1R);
+for i = 1:NUM_IMAGES
+    % read L1 and L1R pan
+    panL1 = imread(strcat('../images/', num2str(i), '/L1/0/image.tif'));
+    panL1R = imread(strcat('../images/', num2str(i), '/L1R/0/image.tif'));
 
     for r = NUM_REF_ROWS_COLS
-        simpleDirL0 = strcat('../images/', num2str(i), '/L0/output_', num2str(r), '/');
-        simpleDirL1 = strcat('../images/', num2str(i), '/L1/output_', num2str(r), '/');
-        enhancedDirL0 = strcat('../images/', num2str(i), '/L0/enhanced_output_', num2str(r), '/');
-        enhancedDirL1 = strcat('../images/', num2str(i), '/L1/enhanced_output_', num2str(r), '/');
-
+        dtwDir = strcat('../images/', num2str(i), '/DTW/', num2str(r), '_refs/');
+        
         % read the MSI bands produced using DTW
         for b = 1:NUM_MSI_BANDS
-            msiDtwSimpleL0(:, :, b) = imread(strcat(simpleDirL0, num2str(b), '.tif'));
-            msiDtwSimpleL1(:, :, b) = imread(strcat(simpleDirL1, num2str(b), '.tif'));
-            msiDtwEnhancedL0(:, :, b) = imread(strcat(enhancedDirL0, num2str(b), '.tif'));
-            msiDtwEnhancedL1(:, :, b) = imread(strcat(enhancedDirL1, num2str(b), '.tif'));
+            msiDtw(:, :, b) = imread(strcat(dtwDir, num2str(b), '.tif'));
         end
         
-        % pansharp DTW outputs
-        dtwSimpleHcsL0 = pansharp_hcs(panL0(MARGIN + 1:end - MARGIN, MARGIN + 1:end - MARGIN), ...
-                                      msiDtwSimpleL0(MARGIN + 1:end - MARGIN, MARGIN + 1:end - MARGIN, :));
+        % pansharp DTW output
+        dtwHcs = pansharp_hcs(panL1(ROW_MARGIN + 1:end - ROW_MARGIN, COL_MARGIN + 1:end - COL_MARGIN), ...
+                              msiDtw(ROW_MARGIN + 1:end - ROW_MARGIN, COL_MARGIN + 1:end - COL_MARGIN, :));
         
-        dtwSimpleHcsL1 = pansharp_hcs(panL1(MARGIN + 1:end - MARGIN, MARGIN + 1:end - MARGIN), ...
-                                      msiDtwSimpleL1(MARGIN + 1:end - MARGIN, MARGIN + 1:end - MARGIN, :));
-        
-        dtwEnhancedHcsL0 = pansharp_hcs(panL0(MARGIN + 1:end - MARGIN, MARGIN + 1:end - MARGIN), ...
-                                        msiDtwEnhancedL0(MARGIN + 1:end - MARGIN, MARGIN + 1:end - MARGIN, :));
-        
-        dtwEnhancedHcsL1 = pansharp_hcs(panL1(MARGIN + 1:end - MARGIN, MARGIN + 1:end - MARGIN), ...
-                                        msiDtwEnhancedL1(MARGIN + 1:end - MARGIN, MARGIN + 1:end - MARGIN, :));
-        
-        % write sharpened images
-        imwrite(uint16(dtwSimpleHcsL0), strcat(simpleDirL0, 'pansharp_hcs.tif'));                            
-        imwrite(uint16(dtwSimpleHcsL1), strcat(simpleDirL1, 'pansharp_hcs.tif'));
-        imwrite(uint16(dtwEnhancedHcsL0), strcat(enhancedDirL0, 'pansharp_hcs.tif'));
-        imwrite(uint16(dtwEnhancedHcsL1), strcat(enhancedDirL1, 'pansharp_hcs.tif'));
-        
-        % write thumbnails
-        imwrite(32 * imresize(uint16(dtwSimpleHcsL0), 0.2), strcat(simpleDirL0, 'pansharp_hcs_thumbnail.tif'));
-        imwrite(32 * imresize(uint16(dtwSimpleHcsL1), 0.2), strcat(simpleDirL1, 'pansharp_hcs_thumbnail.tif'));
-        imwrite(32 * imresize(uint16(dtwEnhancedHcsL0), 0.2), strcat(enhancedDirL0, 'pansharp_hcs_thumbnail.tif'));
-        imwrite(32 * imresize(uint16(dtwEnhancedHcsL1), 0.2), strcat(enhancedDirL1, 'pansharp_hcs_thumbnail.tif'));
+        % write output and its thumbnail
+        imwrite(uint16(dtwHcs), strcat(dtwDir, 'dtw_hcs.tif'));
+        imwrite(32 * imresize(uint16(dtwHcs), 0.2), strcat(dtwDir, 'dtw_hcs_thumbnail.tif'));
     end
     
     % read the MSI bands produced using SIFT
@@ -82,8 +52,8 @@ for i = 11:NUM_IMAGES
     
     % pansharp SIFT output and write sharpened image & thumbnail
     siftHcs = uint16(pansharp_hcs(panL1R, msiSift));
-    imwrite(siftHcs, strcat(msiSiftDir, 'pansharp_hcs.tif'));
-    imwrite(32 * imresize(siftHcs, 0.2), strcat(msiSiftDir, 'pansharp_hcs_thumbnail.tif'));
+    imwrite(siftHcs, strcat(msiSiftDir, 'sift_hcs.tif'));
+    imwrite(32 * imresize(siftHcs, 0.2), strcat(msiSiftDir, 'sift_hcs_thumbnail.tif'));
     
     fprintf('Pansharpening successful for image #%d.\n', i);
 end
